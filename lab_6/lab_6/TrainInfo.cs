da27c16cc8b7b497f44e0cf4fc5df95309243ec7
Avtomatplis <119958728+Avtomatplis@users.noreply.github.com>
2023-05-06 11:52:09 +0300

    Add files via upload

---
+++ lab_6/lab_6/TrainInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab_6
+{
+    public class TrainInfo
+    {
+        public int Id { get; set; }
+        public string Destination { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int PlatformNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"Поезд номер {Id} выдвигается в {Destination} с платформы #{PlatformNumber}. Дата и время: {DepartureTime}";
+        }
+
+        public TrainInfo(int id, string destination, int platformNumber)
+        {
+            this.Id = id ;
+            this.Destination = destination ;
+            this.PlatformNumber = platformNumber ;
+            this.DepartureTime = DateTime.Now ;
+        }
+
+    }
+}
