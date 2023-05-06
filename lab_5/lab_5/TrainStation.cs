4c55b307f6791642e5e27829476707a6846fbfd9
Avtomatplis <119958728+Avtomatplis@users.noreply.github.com>
2023-05-06 11:48:59 +0300

    Add files via upload

---
+++ lab_5/lab_5/TrainStation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5
+{
+    public class TrainStation:ITrainStation
+    {
+        /// <summary>
+        /// Название станции
+        /// </summary>
+        public string Name { get; set; } = "";
+        /// <summary>
+        /// Количество сотрудников
+        /// </summary>
+        public int EmployeesInStation { get; set; }
+        /// <summary>
+        /// Количество поездов
+        /// </summary>
+        public int NumberOfTrains { get; set; }
+        /// <summary>
+        ///   Директор станции
+        /// </summary>
+        public string DirectorName { get; set; } = "";
+
+        static public int TrainStationCount { get; set; }
+
+        public TrainStation(string Name, int EmployeesInStation, int NumberOfTrain, string DirectorName) 
+        {
+            this.Name = Name;
+            this.EmployeesInStation = EmployeesInStation;
+            this.NumberOfTrains = NumberOfTrain;
+            this.DirectorName = DirectorName;
+            TrainStationCount++;
+        }
+
+        public TrainStation() { TrainStationCount++; }
+
+        public string GetInfo()
+        {
+          return ($"Name: {Name},\nEmployeesInStation: {EmployeesInStation},\nNumberOfTrains: {NumberOfTrains},\n" +
+                $"DirectorName: {DirectorName}\n") ;
+        }
+
+       public ITrainStation Clone()
+        {
+            return new TrainStation(this.Name,this.EmployeesInStation,this.NumberOfTrains,this.DirectorName);
+        }
+    }
+}
